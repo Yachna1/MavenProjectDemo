cb9c97afade54c9efe709facaf82778090575210
Yachna1 <ysharma2710@gmail.com>
2020-05-12 13:34:40 +0530

    first Commit

---
+++ src/main/java/com/MavenProjectdemo/App.java
@@ -0,0 +1,13 @@
+package com.MavenProjectdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
