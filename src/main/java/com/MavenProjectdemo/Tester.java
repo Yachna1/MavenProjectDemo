e45fe39892ff9798695d231813e2fff67f2ced60
Yachna1 <ysharma2710@gmail.com>
2020-06-01 13:11:16 +0530

    Engineer commit updated

--- src/main/java/com/MavenProjectdemo/Tester.java
+++ src/main/java/com/MavenProjectdemo/Tester.java
@@ -3,7 +3,7 @@ package com.MavenProjectdemo;
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
             System.out.println("hello");
 	}
 
