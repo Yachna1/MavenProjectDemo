b8733f2bd250907a1a5e6685b229f8f48e905cfb
Yachna1 <ysharma2710@gmail.com>
2020-06-05 12:40:04 +0530

    Drivers Commit

--- src/main/java/com/MavenProjectdemo/Selenium.java
+++ src/main/java/com/MavenProjectdemo/Selenium.java
@@ -1,9 +1,25 @@
 package com.MavenProjectdemo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Selenium {
 
 	public static void main(String[] args) {
 		System.out.println("selenium is Automation Tool");
+
+			System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+			WebDriver d = new ChromeDriver();
+			d.get("https://accounts.google.com/signup ");
+			d.findElement(By.cssSelector("#firstName")).click();
+			d.findElement(By.cssSelector("#lastName")).click();
+			d.findElement(By.cssSelector("#username")).click();
+			String css ="input[type ='password']";
+			d.findElement(By.cssSelector(css)).click();
+			String CSS ="input[name ='ConfirmPasswd']";
+			d.findElement(By.cssSelector(CSS)).click();
+			
 
 	}
 
